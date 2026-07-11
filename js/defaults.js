36b57d4b6ae92c2ead766e37a2a7c53a1ccd20a4
Valentin <valent-me@protonmail.com>
2026-07-11 14:45:12 +0300

    v1.5.3

--- js/defaults.js
+++ js/defaults.js
@@ -2,7 +2,7 @@
 
 const DEFAULT_PARAMS = {};
 
-DEFAULT_PARAMS.programVersion = "1.5.2";
+DEFAULT_PARAMS.programVersion = "1.5.3";
 DEFAULT_PARAMS.fileFormatVersion = "21";
 
 DEFAULT_PARAMS.maxPatternSteps = 64;
